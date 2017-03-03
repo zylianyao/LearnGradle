41204bd74cd16a2f3edca3b754c0a38341de8dd6
三产 <13522955396@163.com>
2017-03-03 16:13:38 +0800

    命令

---
+++ HelloWorld/查看所有task.bat
@@ -0,0 +1,5 @@
+@echo off
+:1
+title Gradle学习-HelloWorld
+
+echo= &&echo gradle -q tasks 执行结果：&& echo =============================== && gradle -q tasks && echo= &&echo gradle  tasks 执行结果：&& echo =============================== && gradle  tasks && echo= &&echo gradle  tasks --all 执行结果：&& echo =============================== && gradle  tasks --all&& pause
